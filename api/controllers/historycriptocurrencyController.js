1ee1cc607c7194c7f158b8a5c987297415307f4d
Vinícius Trainotti <vtrainotti@live.com>
2021-08-13 20:45:44 -0300

    ADD bot interactive to history send message chat Telegram

--- api/controllers/historycriptocurrencyController.js
+++ api/controllers/historycriptocurrencyController.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const env = require('../../.env');
+
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(env.token);
 
 const HistoryCryptocurrency = require('../models/historycriptocurrencyModel');
 
@@ -14,9 +19,26 @@ class oHistoryCriptocurrency{
 
         try {
             const returnData = await HistoryCryptocurrency.find({ 'date': {"$gte": from_date}});
+            for(let coin of returnData){
+                let date = self.dataAtualFormatada(coin.date);
+                let price = coin.price.toLocaleString('pt-br', {minimumFractionDigits: 2});
+                console.log('date', date);
+                //console.log('date format', date);
+                let messageBot = 'Data: '
+                                + date
+                                + '\n'
+                                + 'Criptomoeda: '
+                                + coin.symbol
+                                + '\n' 
+                                + 'Preço: R$ '
+                                + price
+                                +'\n\n';
+                console.log('messageBot', messageBot);
+                bot.telegram.sendMessage("-497749810", messageBot);
+            }
             response = { 'status': 200, 'data': { 'message' : returnData } }
         } catch (error) {
-            console.log('save error', error);
+            console.log('history all error', error);
             response = { 'status': 500, 'data': { 'message' : error } }
         }
         
@@ -27,6 +49,16 @@ class oHistoryCriptocurrency{
         let date = new Date();
         date.setHours(0, 0, 0, 0);
         return date;
+    }
+
+    dataAtualFormatada(date){
+        var data = new Date(date),
+            dia  = data.getDate().toString(),
+            diaF = (dia.length == 1) ? '0'+dia : dia,
+            mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+            mesF = (mes.length == 1) ? '0'+mes : mes,
+            anoF = data.getFullYear();
+        return diaF+"/"+mesF+"/"+anoF;
     }
 }
 
